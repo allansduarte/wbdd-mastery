1d17d1b17869651f4c9aae63aa17257d305aeb83
Allan <allan.sduarte@gmail.com>
2021-03-28 17:14:27 -0300

    Add schedule feature for quizzes

--- lib/mastery.ex
+++ lib/mastery.ex
@@ -3,7 +3,6 @@ defmodule Mastery do
   alias Mastery.Boundary.{TemplateValidator, QuizValidator}
   alias Mastery.Core.Quiz
 
-
   def build_quiz(fields) do
     with :ok <- QuizValidator.errors(fields),
          :ok <- QuizManager.build_quiz(fields),
@@ -33,5 +32,13 @@ defmodule Mastery do
 
   def answer_question(name, answer) do
     QuizSession.answer_question(name, answer)
+  end
+
+  def schedule_quiz(quiz, templates, start_at, end_at) do
+    with :ok <- QuizValidator.errors(quiz),
+         true <- Enum.all?(templates, &(:ok == TemplateValidator.errors(&1))),
+         :ok <- Proctor.schedule_quiz(quiz, templates, start_at, end_at),
+         do: :ok,
+         else: (error -> error)
   end
 end
